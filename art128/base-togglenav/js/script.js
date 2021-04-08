7d36748052fc6e5582c04bf2d781e029fbee9685
LilyRi <77715752+LilyRi@users.noreply.github.com>
2021-04-08 09:39:41 -1000

    completed

---
+++ art128/base-togglenav/js/script.js
@@ -0,0 +1,17 @@
+ 
+document.addEventListener("DOMContentLoaded", function() {
+  
+    //TOGGLE MENU NAV VIA ADDING/REMOVING A DATA-ATTRIBUTE
+    const menubutton = document.querySelector('.menu-button');
+    const menunav = document.querySelector('.toggle-nav'); 
+    menubutton.addEventListener('click', function() {
+      if (menunav.getAttribute('data-navstate') === 'open') { 
+            // if ture do this
+          menunav.setAttribute('data-navstate', 'closed');
+      } else {
+            // else (if false) do this
+          menunav.setAttribute('data-navstate', 'open');
+      }
+    });
+  
+  });
