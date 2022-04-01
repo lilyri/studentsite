c1dd30942d0f3768ced5f0b6b57367f1dae81aaa
LilyRi <77715752+LilyRi@users.noreply.github.com>
2022-04-01 11:07:28 -1000

    added the images for slideshow

--- art128/hn-coded-draft1/js/script.js
+++ art128/hn-coded-draft1/js/script.js
@@ -1,24 +1,8 @@
  
 document.addEventListener("DOMContentLoaded", function() {
   
-  var slideIndex = 1;
-  showDivs(slideIndex);
-  
-  function plusDivs(n) {
-    showDivs(slideIndex += n);
-  }
-  
-  function showDivs(n) {
-    var i;
-    var x = document.getElementsByClassName("mySlides");
-    if (n > x.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = x.length} ;
-    for (i = 0; i < x.length; i++) {
-      x[i].style.display = "none";
-    }
-    x[slideIndex-1].style.display = "block";
-  }
 
+  
     //TOGGLE MENU NAV VIA ADDING/REMOVING A DATA-ATTRIBUTE
     const menubutton = document.querySelector('.menu-button');
     const menunav = document.querySelector('.toggle-nav'); 
